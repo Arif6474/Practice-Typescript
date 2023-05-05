edea78a7ea96ede256daad44672f4e98f6d197d8
Arif6474 <arifulislam64743@gmail.com>
2023-05-05 21:01:00 +0600

    generic function

---
+++ src/Module3/generic-function.ts
@@ -0,0 +1,22 @@
+// 3-5: Generic in Function
+
+const createArray = (params : string) : string[]=>{
+  return [params]
+}
+const createArray2 = <X, Y> (params : X, params2 : Y) : [X, Y]=>{
+  return [params , params2]
+}
+
+const result = createArray('Arif Islam')
+
+const result2 = createArray2<string, boolean>('Arif Islam', true)
+interface personInterface {
+    age: number;
+    salary: number;
+}
+const result3 = createArray2<string, personInterface>('Arif Islam', {
+    age: 25,
+    salary:25000
+})
+
+console.log(result3);
