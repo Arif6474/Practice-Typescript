475aaf57ff7d0c3857db9898d15ba6d57385b0e3
Arif6474 <arifulislam64743@gmail.com>
2023-05-07 00:10:55 +0600

    class , object , parameter

---
+++ src/module4/object.ts
@@ -0,0 +1,30 @@
+// 4-2: How to Create Class, Object , Parameter Properties
+
+// class Animal {
+//     name: string;
+//     species: string;
+//     sound: string;
+
+//     constructor(name: string, species: string, sound: string) {
+//         this.name = name;
+//         this.species = species;
+//         this.sound = sound;
+//     }
+//     makeSound() {
+//         console.log(`The ${this.name} says ${this.sound}`);
+        
+//     }
+// }
+class Animal {
+  
+    constructor(public name: string,public species: string,public sound: string) {
+      
+    }
+    makeSound() {
+        console.log(`The ${this.name} says ${this.sound}`);
+        
+    }
+}
+
+const dog = new Animal('Menu', 'dog' ,'ghew ghew')
+dog.makeSound()
